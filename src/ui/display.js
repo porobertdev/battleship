38d3fb7e2be98da8947e039659bf84dce8ac3d55
Robert P. <porobertdev@gmail.com>
2024-09-10 19:18:48 +0300

    Move code related to DOM to its own module

---
+++ src/ui/display.js
@@ -0,0 +1,34 @@
+import Ship from '../game/Ship';
+import { getCoordinate } from './utils';
+
+function displayShip(player) {
+    const boardSelector = player.type === 'computer' ? 'player1' : 'player2';
+    const squares = document.querySelectorAll(
+        `.${boardSelector} .grid div[class^="square"]`
+    );
+    squares.forEach((s) => s.addEventListener('click', placeShipHandler));
+
+    function placeShipHandler(event) {
+        const target = event.target;
+        console.log('🚀 ~ placeShipHandler ~ target:', target);
+
+        if (
+            player.gameboard.ships.length < 5 &&
+            !target.classList.toString().includes('ship')
+        ) {
+            const row = getCoordinate(target.parentElement);
+            const col = getCoordinate(target);
+
+            player.gameboard.placeShip(new Ship(target.ship.name), [row, col]);
+            target.siblings.forEach((el) =>
+                el.classList.add('ship', target.ship.name)
+            );
+
+            console.log(player.gameboard.ships);
+        } else {
+            alert('no more ships left');
+        }
+    }
+}
+
+export { displayShip };
